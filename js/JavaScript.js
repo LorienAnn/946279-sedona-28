03caf0c32432328d8cfc3765d755b071cc55225d
LorienAnn <lorienann@gmail.com>
2020-06-25 17:09:23 +0300

    декоративные элементы завершение

---
+++ js/JavaScript.js
@@ -0,0 +1,6 @@
+var button = document.querySelector(".button-brown");
+var form = document.querySelector(".form-hotels");
+button.onclick = function() {
+    form.classList.toggle("form-hotels-none");
+  };
+ 
