71d893f62f97b03ba9660c214a03d8f657ed4da3
LorienAnn <lorienann@gmail.com>
2020-07-01 21:58:22 +0300

    правки

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,6 +1,31 @@
-var button = document.querySelector(".button-brown");
+/*var button = document.querySelector(".button-brown");
 var form = document.querySelector(".form-hotels");
+var search = document.querySelector("input-form");
 button.onclick = function() {
     form.classList.toggle("form-hotels-none");
-  };
+    search.focus();
+  };*/
  
+var button = document.querySelector(".button-brown");
+var form = document.querySelector(".form-hotels");
+var arrivals = document.querySelector(".input-form-arrivals");
+var departures = document.querySelector(".input-form-departures");
+var form = document.querySelector (".form-hotels");
+ 
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  form.classList.toggle("form-hotels-none");
+  arrivals.focus(); 
+});
+
+button.addEventListener("click", function (evt) {    /* Не получается :( */
+  evt.preventDefault();
+  form.classList.toggle ("form-show");
+  arrivals.focus(); 
+});
+
+form.addEventListener("submit", function (evt) {     
+  if(!arrivals.value || !departures.value) {
+  evt.preventDefault();
+} 
+});
