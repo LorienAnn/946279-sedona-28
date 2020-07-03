cc759cfa3a36077ca1eeda0211d99a4cf55d091b
LorienAnn <lorienann@gmail.com>
2020-07-03 19:06:48 +0300

    финальные правки

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
- 
 var button = document.querySelector(".button-brown");
 var form = document.querySelector(".form-hotels");
 var arrivals = document.querySelector(".input-form-arrivals");
 var departures = document.querySelector(".input-form-departures");
+var adults = document.querySelector(".adult-label");
 
 form.classList.toggle("form-hotels-none"); 
 button.addEventListener("click", function (evt) {
@@ -11,11 +11,22 @@ button.addEventListener("click", function (evt) {
   arrivals.focus(); 
 });
 
- 
 form.addEventListener("submit", function (evt) {     
-  if(!arrivals.value || !departures.value) {
+  if(!arrivals.value || !departures.value || !adults.value) {
   evt.preventDefault(); 
-  form.classList.add("form-error");  
-  arrivals.focus();             
-} 
+  form.classList.remove("form-error");
+  form.offsetWidth = form.offsetWidth;
+  form.classList.add("form-error");
+  arrivals.focus(); 
+  console.log("Необходимо ввести логин и пароль");
+  }           
+});
+
+form.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+  if (form.classList.contains("form-hotels")) {
+      evt.preventDefault();
+      form.classList.toggle("form-hotels-none");
+    }
+  }
 });
