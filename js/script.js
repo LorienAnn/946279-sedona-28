156e813be1790d0ec7dccaef8180a0002e846391
LorienAnn <lorienann@gmail.com>
2020-07-04 19:26:33 +0300

    финал

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ var form = document.querySelector(".form-hotels");
 var arrivals = document.querySelector(".input-form-arrivals");
 var departures = document.querySelector(".input-form-departures");
 var adults = document.querySelector(".adult-label");
+var children = document.querySelector(".children-label")
 
 form.classList.toggle("form-hotels-none"); 
 button.addEventListener("click", function (evt) {
@@ -12,21 +13,19 @@ button.addEventListener("click", function (evt) {
 });
 
 form.addEventListener("submit", function (evt) {     
-  if(!arrivals.value || !departures.value || !adults.value) {
-  evt.preventDefault(); 
-  form.classList.remove("form-error");
-  form.offsetWidth = form.offsetWidth;
-  form.classList.add("form-error");
-  arrivals.focus(); 
-  console.log("Необходимо ввести логин и пароль");
-  }           
+  if(!arrivals.value || !departures.value || !adults.value || !children.value) {
+    evt.preventDefault(); 
+    form.classList.remove("form-error");
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add("form-error");
+    console.log("Необходимо ввести логин и пароль");
+    }           
 });
 
 form.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
-  if (form.classList.contains("form-hotels")) {
-      evt.preventDefault();
-      form.classList.toggle("form-hotels-none");
-    }
+    if (form.classList.contains("form-hotels")) {
+        form.classList.toggle("form-hotels-none");
+        }
   }
 });
